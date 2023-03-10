9fd1e4a0187e112698c0c665a1aa049d0a23cae2
Fernando Catu <fercatuo@gmail.com>
2023-03-09 19:10:51 -0600

    Blog de videojuegos V1

---
+++ includes/pie.php
@@ -0,0 +1,11 @@
+
+			<div class="clearfix"></div>
+		</div> <!-- fin contenedor -->
+
+		<!-- PIE DE PÁGINA -->
+		<footer id="pie">
+			<p>Desarrollado por Grupo 5 &copy; 2021</p>
+		</footer>
+		
+	</body>
+</html>
